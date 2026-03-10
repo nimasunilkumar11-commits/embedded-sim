201ee13176b116495cfd69bdca17341117c91140
Nima Sunilkumar <nima.sunilkumar11@gmail.com>
2026-03-10 20:04:13 +0100

    Integrate scheduler into main loop

--- src/main.c
+++ src/main.c
@@ -1,30 +1,17 @@
-#include <stdio.h>
-#include <unistd.h>
 #include "sensor.h"
-#include "controller.h"
 #include "logger.h"
+#include "scheduler.h"
 
-
-
-int main() {
+int main()
+{
+    init_sensors();
     init_logger();
 
-    init_sensors();
-
-    while (1) {
-
-        SensorData data = read_sensors();
-
-        printf("Temperature: %.2f C\n", data.temperature);
-        printf("Pressure: %.2f hPa\n", data.pressure);
-        printf("Motion: %s\n", data.motion ? "Detected" : "None");
-
-        process_data(data);
-
-        printf("-----------------------------\n");
-
-        sleep(2);
+    while(1)
+    {
+        scheduler_run();
     }
+
     close_logger();
     return 0;
 }
