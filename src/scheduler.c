2795d430b9e6f23ef67502275c9d1b8f5932f7f8
Nima Sunilkumar <nima.sunilkumar11@gmail.com>
2026-03-11 19:39:58 +0100

    Update scheduler to feed data into statistics module

--- src/scheduler.c
+++ src/scheduler.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
 #include "sensor.h"
 #include "controller.h"
+#include "statistics.h"
 
 void scheduler_run(void)
 {
     SensorData data = read_sensors();
+    stats_update(data.temperature);
     process_data(data);
 
     sleep(2);
