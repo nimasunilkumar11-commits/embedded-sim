43140f559eb6c3f31ceb048d8ca231382e2c46e5
Nima Sunilkumar <nima.sunilkumar11@gmail.com>
2026-03-11 19:41:48 +0100

    Initialize statistics module during system startup

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include "sensor.h"
 #include "logger.h"
 #include "scheduler.h"
+#include "statistics.h"
+stats_init();
 
 int main()
 {
     init_sensors();
     init_logger();
+    load_config();
+    stats_init();
 
     while(1)
     {
