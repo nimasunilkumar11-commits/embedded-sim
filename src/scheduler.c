47bb11ffb0e1571449285312fe5104275b480bf5
Nima Sunilkumar <nima.sunilkumar11@gmail.com>
2026-03-13 18:57:08 +0100

    Integrate command interface into scheduler

--- src/scheduler.c
+++ src/scheduler.c
@@ -2,12 +2,17 @@
 #include "sensor.h"
 #include "controller.h"
 #include "statistics.h"
+#include "command.h"
 
 void scheduler_run(void)
 {
     SensorData data = read_sensors();
+
     stats_update(data.temperature);
+
     process_data(data);
+
+    command_check();
 
     sleep(2);
 }
